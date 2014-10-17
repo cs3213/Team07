52d615421ba0e7938838bfd87fee1f4258408fb6
Melvin Lee <zy@zy.sg>
2014-10-17 19:07:03 +0800

    Fixed background select menu not displaying correctly in Yosemite.

--- client/controllers/application.js
+++ client/controllers/application.js
@@ -1,50 +1,50 @@
 App.ApplicationController = Ember.ObjectController.extend({
-	isLoggedIn: false,
+    isLoggedIn: false,
 
 
-	init: function () {
-		this._super();
-		this.send('getSession');
-	},
+    init: function () {
+        this._super();
+        this.send('getSession');
+    },
 
     actions: {
         'login': function() {
-        	var controller = this;
+            var controller = this;
             $.getJSON("/auth/google/",function(result){
-            	controller.send('getSession');
+                controller.send('getSession');
             });
         },
         'logout': function() {
-        	var controller = this;
+            var controller = this;
             $.getJSON("/logout",function(result){
-            	controller.send('getSession');
+                controller.send('getSession');
             });
         },
 
-	    getSession: function() {
-	        var controller = this;
-	        $.getJSON('/user').done(function(user) {
-	            controller.set('isLoggedIn', user.authenticated);
-	        }).error(function(){
-	            Ember.Logger.log('error occur');
-	        });
-	    },
+        getSession: function() {
+            var controller = this;
+            $.getJSON('/user').done(function(user) {
+                controller.set('isLoggedIn', user.authenticated);
+            }).error(function(){
+                Ember.Logger.log('error occur');
+            });
+        },
 
-	    saveProject: function() {
-	        var controller = this;
-	        $.getJSON('/save',
-	        		{
-	        			// not sure how to get the required datas from models
-	        			// testing out the code
-		        		user: this.get('user'), 
-						projectId: this.get('project.uuid'),
-						projectJson: this.get('project')
-					}
-	        	).done(function(user) {
-	       	  		//TODO show save success
-	       	   }).error(function(){
-	            Ember.Logger.log('error occur');
-	        });
-	    }
-	}
+        saveProject: function() {
+            var controller = this;
+            $.getJSON('/save',
+                    {
+                        // not sure how to get the required datas from models
+                        // testing out the code
+                        user: this.get('user'), 
+                        projectId: this.get('project.uuid'),
+                        projectJson: this.get('project')
+                    }
+                ).done(function(user) {
+                    //TODO show save success
+               }).error(function(){
+                Ember.Logger.log('error occur');
+            });
+        }
+    }
 });
